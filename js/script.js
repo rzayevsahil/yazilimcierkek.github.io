f12dbd1b43c307ed80f62c9e35a3d97b2cab3a0f
Javid Ismayıl <61595060+yazilimcierkek@users.noreply.github.com>
2021-05-26 23:19:18 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,19 @@ window.addEventListener("load",()=>{
     setTimeout(()=>{
         document.querySelector(".page-loader").style.display= "none";
     },600);
+    $(function() {
+        $('#WAButton').floatingWhatsApp({
+          phone: '1231231231', //WhatsApp Business phone number International format-
+          //Get it with Toky at https://toky.co/en/features/whatsapp.
+          headerTitle: 'Chat with us on WhatsApp!', //Popup Title
+          popupMessage: 'Hello, how can we help you?', //Popup Message
+          showPopup: true, //Enables popup display
+          buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
+          //headerColor: 'crimson', //Custom header color
+          //backgroundColor: 'crimson', //Custom background button color
+          position: "right"    
+        });
+      });	
     
 });
 
