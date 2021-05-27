c32904d705510a305050049ec3f9a2952c1e53ef
Javid Ismayıl <61595060+yazilimcierkek@users.noreply.github.com>
2021-05-27 14:38:59 +0400

    Add files via upload

---
+++ Demo/EmailVer/script.js
@@ -0,0 +1,19 @@
+
+
+function validate(){
+    const form =document.getElementById('form');
+    const email =document.getElementById('email').value;
+    const pattern =/^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+
+    if(email.match(pattern)){
+        form.classList.add('valid');
+        form.classList.remove('invalid');
+    }else{
+        form.classList.add('invalid');
+        form.classList.remove('valid');
+    }
+    if(email===""){
+        form.classList.remove('invalid');
+        form.classList.remove('valid');
+    }
+}
